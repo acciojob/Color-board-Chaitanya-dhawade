03652e50f54855606e54f13084d29648b2a817ad
Chaitanya-dhawade <chaitanyadhawade23@gmail.com>
2026-06-16 23:22:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+const container = document.getElementById("container");
+
+// create 800 squares
+for (let i = 0; i < 800; i++) {
+
+    const square = document.createElement("div");
+    square.classList.add("square");
+
+    square.addEventListener("mouseover", () => {
+        const color = getRandomColor();
+        square.style.background = color;
+
+        setTimeout(() => {
+            square.style.background = "#1d1d1d";
+        }, 1000);
+    });
+
+    container.appendChild(square);
+}
+
+// random color generator
+function getRandomColor() {
+    const colors = [
+        "#ff4d4d", "#4dff4d", "#4d4dff",
+        "#ffff4d", "#ff4dff", "#4dffff",
+        "#ff884d", "#8d4dff"
+    ];
+
+    return colors[Math.floor(Math.random() * colors.length)];
+}
